f47979d68b8a2108e0b1d8ed9b3c5a4c5b2bebf4
tom <abc@abc.com>
2021-01-02 15:53:06 +0800

    add trace bar

---
+++ UI/MyTraceBar.c
@@ -0,0 +1,9 @@
+/*
+ * MyTraceBar.c
+ *
+ *  Created on: 2021年1月2日
+ *      Author: tom
+ */
+
+
+#include "MyTraceBar.h"
---
+++ UI/MyTraceBar.h
@@ -0,0 +1,13 @@
+/*
+ * MyTraceBar.h
+ *
+ *  Created on: 2021年1月2日
+ *      Author: tom
+ */
+
+#ifndef UI_MYTRACEBAR_H_
+#define UI_MYTRACEBAR_H_
+
+
+
+#endif /* UI_MYTRACEBAR_H_ */
